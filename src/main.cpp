8d83ae7657cc94fb0bf4bdc6413dd08308f1b66c
Albert Nadal Garriga <anadalg@gmail.com>
2026-07-28 18:29:32 +0200

    Some physics and collisions prove of concept

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,40 @@
-#include <raylib/raylib.h>
 #include <box3d/box3d.h>
+#include <raylib/raylib.h>
 
 constexpr float SCR_WIDTH = 640.0f;
 constexpr float SCR_HEIGHT = 480.0f;
 constexpr const char *WINDOW_TITLE = "SkyRoads Clone";
 
 int main() {
-  // Construct a world object, which will hold and simulate the rigid bodies.
   b3WorldDef worldDef = b3DefaultWorldDef();
-  worldDef.gravity = (b3Vec3){0.0f, -10.0f, 0.0f};
+  worldDef.gravity = (b3Vec3){0.0f, -9.80665f, 0.0f};
 
   b3WorldId worldId = b3CreateWorld(&worldDef);
 
-  // Define the ground body.
-  b3BodyDef groundBodyDef = b3DefaultBodyDef();
-  groundBodyDef.position = (b3Pos){0.0f, -10.0f, 0.0f};
+  Vector3 laneSize = (Vector3){3.0f, 1.0f, 6.0f};
+  Vector3 lanePos = (Vector3){5.0f, 0.5f, 0.0f};
 
-  // Call the body factory which allocates memory for the ground body
-  // from a pool and creates the ground box shape (also from a pool).
-  // The body is also added to the world.
-  b3BodyId groundId = b3CreateBody(worldId, &groundBodyDef);
+  b3BodyDef laneBodyDef = b3DefaultBodyDef();
+  laneBodyDef.type = b3_staticBody;
+  laneBodyDef.position = (b3Pos){lanePos.x, lanePos.y, lanePos.z};
+  b3BodyId laneBodyId = b3CreateBody(worldId, &laneBodyDef);
+  b3BoxHull laneStaticBox = b3MakeBoxHull(laneSize.x * 0.5f, laneSize.y * 0.5f, laneSize.z * 0.5f);
+  b3ShapeDef laneShapeDef = b3DefaultShapeDef();
+  laneShapeDef.baseMaterial.friction = 0.3f;
+  laneShapeDef.density = 0.0f; // Set the box density to be non-zero, so it will be dynamic.
+  b3CreateHullShape(laneBodyId, &laneShapeDef, &laneStaticBox.base);
 
-  // Define the ground box shape. The extents are the half-widths of the box.
-  b3BoxHull groundBox = b3MakeBoxHull(50.0f, 10.0f, 50.0f);
+  Vector3 shipSize = (Vector3){1.0f, 1.0f, 1.0f};
+  Vector3 shipPos = (Vector3){5.5f, 1.5f, 2.0f};
 
-  // Add the box shape to the ground body.
-  b3ShapeDef groundShapeDef = b3DefaultShapeDef();
-  b3CreateHullShape(groundId, &groundShapeDef, &groundBox.base);
-
-  // Define the dynamic body. We set its position and call the body factory.
-  b3BodyDef bodyDef = b3DefaultBodyDef();
-  bodyDef.type = b3_dynamicBody;
-  bodyDef.position = (b3Pos){0.0f, 4.0f, 0.0f};
-
-  b3BodyId bodyId = b3CreateBody(worldId, &bodyDef);
-
-  // Define another box shape for our dynamic body.
-  b3BoxHull dynamicBox = b3MakeCubeHull(1.0f);
-
-  // Define the dynamic body shape
-  b3ShapeDef shapeDef = b3DefaultShapeDef();
-
-  // Set the box density to be non-zero, so it will be dynamic.
-  shapeDef.density = 1.0f;
-
-  // Override the default friction.
-  shapeDef.baseMaterial.friction = 0.3f;
-
-  // Add the shape to the body.
-  b3CreateHullShape(bodyId, &shapeDef, &dynamicBox.base);
+  b3BodyDef shipBodyDef = b3DefaultBodyDef();
+  shipBodyDef.type = b3_dynamicBody;
+  shipBodyDef.position = (b3Pos){shipPos.x, shipPos.y, shipPos.z};
+  b3BodyId shipBodyId = b3CreateBody(worldId, &shipBodyDef);
+  b3BoxHull shipStaticBox = b3MakeBoxHull(shipSize.x * 0.5f, shipSize.y * 0.5f, shipSize.z * 0.5f);
+  b3ShapeDef shipShapeDef = b3DefaultShapeDef();
+  shipShapeDef.density = 1.0f; // Set the box density to be non-zero, so it will be dynamic.
+  b3CreateHullShape(shipBodyId, &shipShapeDef, &shipStaticBox.base);
 
   // Prepare for simulation. Typically we use a time step of 1/60 of a
   // second (60Hz) and 4 sub-steps. This provides a high quality simulation
@@ -56,33 +42,39 @@ int main() {
   float timeStep = 1.0f / 60.0f;
   int subStepCount = 4;
 
-  b3Pos position = b3Body_GetPosition(bodyId);
-  b3Quat rotation = b3Body_GetRotation(bodyId);
-
-  // This is our little game loop.
-  for (int i = 0; i < 90; ++i) {
-    // Instruct the world to perform a single step of simulation.
-    // It is generally best to keep the time step and iterations fixed.
-    b3World_Step(worldId, timeStep, subStepCount);
-
-    // Now print the position and angle of the body.
-    position = b3Body_GetPosition(bodyId);
-    rotation = b3Body_GetRotation(bodyId);
-  }
-
-  // When the world destructor is called, all bodies and joints are freed. This
-  // can create orphaned ids, so be careful about your world management.
-  b3DestroyWorld(worldId);
-
   InitWindow(SCR_WIDTH, SCR_HEIGHT, WINDOW_TITLE);
+  SetTargetFPS(60);
+
+  Camera3D camera = {0};
+  camera.position = (Vector3){5.0f, 4.0f, 10.0f};
+  camera.target = (Vector3){5.0f, 1.0f, 1.0f};
+  camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+  camera.fovy = 45.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
+
+  b3Vec3 shipForce = { 0.0f, 0.0f, -10.0f };
 
   while (!WindowShouldClose()) {
+    b3Body_ApplyForceToCenter(shipBodyId, shipForce, true);        
+    b3World_Step(worldId, timeStep, subStepCount);
+    b3Pos lanePosition = b3Body_GetPosition(laneBodyId);
+    b3Pos shipPosition = b3Body_GetPosition(shipBodyId);
+
     BeginDrawing();
     ClearBackground(BLACK);
+    BeginMode3D(camera);
+      DrawGrid(20, 1.0f);
+      DrawCube((Vector3){lanePosition.x, lanePosition.y, lanePosition.z}, laneSize.x, laneSize.y, laneSize.z, BLUE);
+      DrawCubeWires((Vector3){lanePosition.x, lanePosition.y, lanePosition.z}, laneSize.x, laneSize.y, laneSize.z, BLACK);
+
+      DrawCube((Vector3){shipPosition.x, shipPosition.y, shipPosition.z}, shipSize.x, shipSize.y, shipSize.z, GREEN);
+      DrawCubeWires((Vector3){shipPosition.x, shipPosition.y, shipPosition.z}, shipSize.x, shipSize.y, shipSize.z, BLACK);
+    EndMode3D();
     DrawFPS(16, 16);
     EndDrawing();
   }
 
   CloseWindow();
+  b3DestroyWorld(worldId);
   return 0;
 }
